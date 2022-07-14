2e9065cd3e1d40c5df4efcee06de0411f4012466
ivan <ivan.kavtarev@mail.ru>
2022-07-14 22:14:14 +0300

    remove junk

--- nest_redone/packages/backend/src/usecase/allowed-route/allowed-module.controller.ts
+++ nest_redone/packages/backend/src/usecase/allowed-route/allowed-module.controller.ts
@@ -3,9 +3,6 @@ import { Controller, Get, Inject, Query } from '@nestjs/common';
 import { RequireAuth } from 'src/core/user/user.guard';
 import { IHttpService } from 'src/modules/http-module/http-service.interface';
 import { AllowedUsecase } from './allowed-route.usecase';
-import { right } from '../../../../common/dist/either';
-
-import { Either } from '../../../../common/helpers/either';
 
 @Controller('/')
 export class AllowedController {
