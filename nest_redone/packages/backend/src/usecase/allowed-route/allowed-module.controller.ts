41b42f67a25347a21f277c456fc1519d65e5382c
ivan <ivan.kavtarev@mail.ru>
2022-08-28 12:52:45 +0300

    add minio via AWS

--- nest_redone/packages/backend/src/usecase/allowed-route/allowed-module.controller.ts
+++ nest_redone/packages/backend/src/usecase/allowed-route/allowed-module.controller.ts
@@ -1,9 +1,18 @@
 import { HTTP_SERVICE } from './../../modules/http-module/constants';
-import { Controller, Get, Inject, Query } from '@nestjs/common';
-import { RequireAuth } from '../../../src/core/user/user.guard';
+import {
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { IHttpService } from '../../modules/http-module/http-service.interface';
 import { AllowedUsecase } from './allowed-route.usecase';
-import { mySum } from '@nest_redone/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { S3 } from 'aws-sdk';
 
 @Controller('/')
 export class AllowedController {
@@ -16,8 +25,31 @@ export class AllowedController {
   // @RequireAuth()
   @Get('pes')
   async getMe(@Query('name') name: string) {
-    // const result = await this.usecase.echo(JSON.stringify(name));
-    const result = await this.httpService.get('todos/1');
-    return result;
+    const result = await this.usecase.echo(JSON.stringify(name));
+    const awayResult = await this.httpService.get('todos/1');
+    return { result, awayResult };
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+
+    const s3Stream = new S3({
+      accessKeyId: 'ozontech',
+      secretAccessKey: 'minio123',
+      endpoint: 'http://minio:9000',
+      s3ForcePathStyle: true, // needed with minio?
+      signatureVersion: 'v4',
+    });
+    console.log(file);
+
+    await s3Stream
+      .upload({
+        Bucket: 'test',
+        Key: file.originalname,
+        Body: file.buffer.toString(),
+      })
+      .promise();
   }
 }
