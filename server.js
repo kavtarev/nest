7868767b0351658bab9a62f3e70a7be3dc73682a
ivan <ivan.kavtarev@mail.ru>
2023-01-28 23:22:41 +0300

    add hosts not working

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const http = require('http');
+ const http = require('http');
+//import * as http from 'http';
+
+// fetch('http://nginx')
+// .then(response => response.json())
+// .then(json => console.log(json))
 
 const server = http.createServer((req, res) => {
   res.end('hello')
