9e64ca3aa406297a7aa94596ee305355bff2829e
ivan <ivan.kavtarev@mail.ru>
2023-02-25 00:21:28 +0300

    parser

--- nest_redone/packages/backend/src/usecase/allowed-route/allowed-route.controller.ts
+++ nest_redone/packages/backend/src/usecase/allowed-route/allowed-route.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Query, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { HTTP_SERVICE } from '../../modules/http-module/constants';
 import { IHttpService } from '../../modules/http-module/http-service.interface';
 import { AllowedRouteUsecase } from './allowed-route.usecase';
@@ -12,10 +13,20 @@ export class AllowedRouteController {
   ) {}
 
   @Get('pes')
-  async getMe(@Query('name') name: string) {
+  async getMe(
+    @Query('name') name: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
     const result = await this.usecase.echo(JSON.stringify(name));
     const awayResult = await this.httpService.get('todos/1');
-
+    console.log(req.headers.cookie);
+    
+    console.log(23, req.cookies);
+    res.setHeader('boo', 'poo');
+    res.cookie('token', 'shmoken', { httpOnly: true });
+    res.send('');
+    // console.log(res.status);
     return { result, awayResult };
   }
 }
